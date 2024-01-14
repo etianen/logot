ecc0c197f9de2198e87c581026256860cd8ebe14
Dave Hall <dave@etianen.com>
2024-01-14 17:06:26 +0000

    wip

---
+++ logtest/_compose.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from collections.abc import Sequence
+from typing import Protocol, TypeVar
+
+T = TypeVar("T")
+T_co = TypeVar("T_co", covariant=True)
+
+
+class Composable(Protocol[T_co]):
+    def __init__(self, items: Sequence[T_co]) -> None:
+        ...
+
+    def _decompose(self) -> Sequence[T_co]:
+        ...
+
+
+def compose(cls: type[Composable[T]], a: T, b: T) -> Composable[T]:
+    if isinstance(a, cls) and isinstance(b, cls):
+        return cls((*a._decompose(), *b._decompose()))
+    if isinstance(a, cls):
+        return cls((*a._decompose(), b))
+    if isinstance(b, cls):
+        return cls((a, *b._decompose()))
+    return cls((a, b))
