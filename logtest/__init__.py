5c8aa1a37d008cfb4a948c1990860fb4871270b4
Dave Hall <dave@etianen.com>
2024-01-15 15:21:27 +0000

    wip

---
+++ logtest/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import annotations
+
+from logtest import match as match
---
+++ tests/test_match.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import logging
+
+import logtest
+from tests import createLogRecord
+
+
+def test_level_match_pass() -> None:
+    assert logtest.match.level(logging.INFO).match(createLogRecord(level=logging.INFO))
+
+
+def test_level_match_fail() -> None:
+    assert not logtest.match.level(logging.INFO).match(createLogRecord(level=logging.DEBUG))
+    assert not logtest.match.level(logging.INFO).match(createLogRecord(level=logging.WARN))
+
+
+def test_level_repr() -> None:
+    assert repr(logtest.match.level(logging.INFO)) == "level('INFO')"
