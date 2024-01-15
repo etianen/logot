e8100b38ac2013e7082e484f5b38c55e30895e9b
Dave Hall <dave@etianen.com>
2024-01-15 13:10:13 +0000

    wip

--- logtest/match.py
+++ logtest/match.py
@@ -2,34 +2,21 @@ from __future__ import annotations
 
 import logging
 from abc import ABC, abstractmethod
-from collections.abc import Callable, Iterable, Mapping, Sequence
+from collections.abc import Callable
 from functools import wraps
 from itertools import chain
-from typing import Any, ClassVar, Final
+from typing import Generic
 
 from typing_extensions import Concatenate, ParamSpec, TypeAlias
-
-from logtest._compose import Composable
 
 P = ParamSpec("P")
 
 _MatcherCallable: TypeAlias = Callable[Concatenate[logging.LogRecord, P], bool]
-_MatcherComposer: TypeAlias = Callable[[Iterable[bool]], bool]
-
-
-# TODO: Make all classes other than Matcher private and have repr show the fn name!
-# TODO: Lose all documented attrs
 
 
 class Matcher(ABC):
     __slots__ = ()
 
-    def __and__(self, matcher: Matcher) -> AllMatcher:
-        return AllMatcher._compose(self, matcher)
-
-    def __or__(self, matcher: Matcher) -> AnyMatcher:
-        return AnyMatcher._compose(self, matcher)
-
     @abstractmethod
     def match(self, record: logging.LogRecord) -> bool:
         raise NotImplementedError
@@ -43,110 +30,69 @@ class Matcher(ABC):
         raise NotImplementedError
 
 
-class _ComposableMatcher(Matcher, Composable[Matcher]):
-    __slots__ = ("matchers",)
-
-    _composer: ClassVar[_MatcherComposer]
-    _operator_str: ClassVar[str]
-
-    matchers: Final[Sequence[Matcher]]
-
-    def __init__(self, matchers: Sequence[Matcher]) -> None:
-        self.matchers = matchers
-
-    def _decompose(self) -> Sequence[Matcher]:
-        return self.matchers
-
-    def match(self, record: logging.LogRecord) -> bool:
-        return self.__class__._composer(matcher.match(record) for matcher in self.matchers)
-
-    def __repr__(self) -> str:
-        return f" {self.__class__._operator_str} ".join(map(repr, self.matchers))
-
-    def __str__(self) -> str:
-        return f" {self.__class__._operator_str} ".join(map(str, self.matchers))
-
-
-class AllMatcher(_ComposableMatcher):
-    __slots__ = ()
-    _composer: ClassVar[_MatcherComposer] = all
-    _operator_str: ClassVar[str] = "&"
-
-
-class AnyMatcher(_ComposableMatcher):
-    __slots__ = ()
-    _composer: ClassVar[_MatcherComposer] = any
-    _operator_str: ClassVar[str] = "|"
-
-
-class LevelMatcher(Matcher):
-    __slots__ = ("levelno",)
-
-    levelno: Final[int]
+class _LevelMatcher(Matcher):
+    __slots__ = ("_levelno",)
 
     def __init__(self, levelno: int) -> None:
-        self.levelno = levelno
+        self._levelno = levelno
 
     def match(self, record: logging.LogRecord) -> bool:
-        return record.levelno == self.levelno
+        return record.levelno == self._levelno
 
     def __repr__(self) -> str:
-        # TODO: Show the level name
-        return f"level({self.levelno!r})"
+        return f"level({logging.getLevelName(self._levelno)!r})"
 
     def __str__(self) -> str:
-        return f"[{logging.getLevelName(self.levelno)}]"
+        return f"[{logging.getLevelName(self._levelno)}]"
 
 
-def level(level: int | str) -> LevelMatcher:
-    # Convert `str` levels to `int`.
+def level(level: int | str) -> _LevelMatcher:
+    # Convert `str` level to `int`.
     if isinstance(level, str):
         level = logging.getLevelName(level)
         if not isinstance(level, int):
             raise ValueError(f"Unknown log level: {level}")
     # All done!
-    return LevelMatcher(level)
+    return _LevelMatcher(level)
 
 
-class MessageMatcher(Matcher):
-    __slots__ = ("message",)
-
-    message: Final[str]
+class _MessageMatcher(Matcher):
+    __slots__ = ("_message",)
 
     def __init__(self, message: str) -> None:
-        self.message = message
+        self._message = message
 
     def match(self, record: logging.LogRecord) -> bool:
-        return record.getMessage() == self.message
+        return record.getMessage() == self._message
 
     def __repr__(self) -> str:
-        return f"message({self.message!r})"
+        return f"message({self._message!r})"
 
     def __str__(self) -> str:
-        return self.message
+        return self._message
 
 
-def message(message: str) -> MessageMatcher:
-    return MessageMatcher(message)
+def message(message: str) -> _MessageMatcher:
+    return _MessageMatcher(message)
 
 
-class _CallableMatcher(Matcher):
-    __slots__ = ("fn", "args", "kwargs")
+class _CallableMatcher(Matcher, Generic[P]):
+    __slots__ = ("_fn", "_args", "_kwargs")
 
-    def __init__(self, fn: _MatcherCallable[Any], args: tuple[Any, ...], kwargs: Mapping[str, Any]) -> None:
-        self.fn = fn
-        self.args = args
-        self.kwargs = kwargs
+    def __init__(self, fn: _MatcherCallable[...], args: tuple[object, ...], kwargs: dict[str, object]) -> None:
+        self._fn = fn
+        self._args = args
+        self._kwargs = kwargs
 
     def match(self, record: logging.LogRecord) -> bool:
-        return self.fn(record, *self.args, **self.kwargs)
+        return self._fn(record, *self._args, **self._kwargs)
 
     def __repr__(self) -> str:
         return f"{self}"
 
     def __str__(self) -> str:
-        args_str = ", ".join(chain(map(repr, self.args), (f"{key}={value!r}" for key, value in self.kwargs.items())))
-        return f"{self.fn.__qualname__}({args_str})"
+        args_str = ", ".join(chain(map(repr, self._args), (f"{key}={value!r}" for key, value in self._kwargs.items())))
+        return f"{self._fn.__qualname__}({args_str})"
 
 
 def matcher() -> Callable[[_MatcherCallable[P]], Callable[P, Matcher]]:
