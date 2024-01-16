18f0f1a7119c6787a82e06ba6a7ca71b82e43c20
Dave Hall <dave@etianen.com>
2024-01-16 10:08:43 +0000

    wip

--- logot/_util.py
+++ logot/_util.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import logging
 
 
-def _check_level(level: int | str) -> int:
+def check_level(level: int | str) -> int:
     # Handle `int` level.
     if isinstance(level, int):
         if logging.getLevelName(level).startswith("Level "):
--- tests/test_util.py
+++ tests/test_util.py
@@ -4,30 +4,30 @@ import logging
 
 import pytest
 
-from logot._util import _check_level
+from logot._util import check_level
 
 
 def test_check_level_int_pass() -> None:
-    assert _check_level(logging.INFO) == logging.INFO
+    assert check_level(logging.INFO) == logging.INFO
 
 
 def test_check_level_int_fail() -> None:
     with pytest.raises(ValueError) as ex:
-        _check_level(9999)
+        check_level(9999)
     assert str(ex.value) == "Unknown level: 9999"
 
 
 def test_check_level_str_pass() -> None:
-    assert _check_level("INFO") == logging.INFO
+    assert check_level("INFO") == logging.INFO
 
 
 def test_check_level_str_fail() -> None:
     with pytest.raises(ValueError) as ex:
-        _check_level("BOOM")
+        check_level("BOOM")
     assert str(ex.value) == "Unknown level: 'BOOM'"
 
 
 def test_check_level_type_fail() -> None:
     with pytest.raises(TypeError) as ex:
-        _check_level(1.5)  # type: ignore[arg-type]
+        check_level(1.5)  # type: ignore[arg-type]
     assert str(ex.value) == "Invalid level: 1.5"
