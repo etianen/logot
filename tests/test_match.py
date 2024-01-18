116f70533e0df5feede41480a161d580a3937a7f
Dave Hall <dave@etianen.com>
2024-01-18 18:09:07 +0000

    wip

--- tests/test_match.py
+++ tests/test_match.py
@@ -9,3 +9,11 @@ def test_literal_match_pass() -> None:
 
 def test_literal_match_fail() -> None:
     assert not match._LiteralMatcher("Hello world").match("Boom!")
+
+
+def test_literal_eq_pass() -> None:
+    assert match._LiteralMatcher("Hello world") == match._LiteralMatcher("Hello world")
+
+
+def test_literal_eq_fail() -> None:
+    assert match._LiteralMatcher("Hello world") != match._LiteralMatcher("Boom!")
