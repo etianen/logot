ba73e4549098692e800ff0a02c19e72b0d693c3c
Dave Hall <dave@etianen.com>
2024-01-19 16:26:53 +0000

    wip

--- logot/_util.py
+++ logot/_util.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import logging
 
 
-def to_levelno(level: int | str) -> int:
+def _to_levelno(level: int | str) -> int:
     # Handle `int` level.
     if isinstance(level, int):
         if logging.getLevelName(level).startswith("Level "):
--- tests/test_util.py
+++ tests/test_util.py
@@ -4,30 +4,30 @@ import logging
 
 import pytest
 
-from logot._util import to_levelno
+from logot._util import _to_levelno
 
 
 def test_to_levelno_int_pass() -> None:
-    assert to_levelno(logging.INFO) == logging.INFO
+    assert _to_levelno(logging.INFO) == logging.INFO
 
 
 def test_to_levelno_int_fail() -> None:
     with pytest.raises(ValueError) as ex:
-        to_levelno(9999)
+        _to_levelno(9999)
     assert str(ex.value) == "Unknown level: 9999"
 
 
 def test_to_levelno_str_pass() -> None:
-    assert to_levelno("INFO") == logging.INFO
+    assert _to_levelno("INFO") == logging.INFO
 
 
 def test_to_levelno_str_fail() -> None:
     with pytest.raises(ValueError) as ex:
-        to_levelno("BOOM")
+        _to_levelno("BOOM")
     assert str(ex.value) == "Unknown level: 'BOOM'"
 
 
 def test_to_levelno_type_fail() -> None:
     with pytest.raises(TypeError) as ex:
-        to_levelno(1.5)  # type: ignore[arg-type]
+        _to_levelno(1.5)  # type: ignore[arg-type]
     assert str(ex.value) == "Invalid level: 1.5"
