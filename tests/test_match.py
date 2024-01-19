258692695abe9e7918ddd062340f116c1790cd6e
Dave Hall <dave@etianen.com>
2024-01-19 17:07:46 +0000

    wip

--- tests/test_match.py
+++ tests/test_match.py
@@ -5,6 +5,14 @@ import pytest
 from logot._match import compile
 
 
-@pytest.mark.parametrize(("pattern", "msg"), (("foo %s baz", "foo bar baz"),))
+@pytest.mark.parametrize(
+    ("pattern", "msg"),
+    (
+        ("foo %c baz", "foo b baz"),
+        ("foo %r baz", "foo bar baz"),
+        ("foo %s baz", "foo bar baz"),
+        ("foo %a baz", "foo bar baz"),
+    ),
+)
 def test_match(pattern: str, msg: str) -> None:
-    assert compile(pattern).match(msg) is not None
+    assert compile(pattern).fullmatch(msg) is not None
