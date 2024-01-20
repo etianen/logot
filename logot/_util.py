41d0295aa9b98c6d79687b4309a3d16c5716a7c5
Dave Hall <dave@etianen.com>
2024-01-20 17:14:29 +0000

    Added `LevelNo` newtype

--- logot/_util.py
+++ logot/_util.py
@@ -1,19 +1,23 @@
 from __future__ import annotations
 
 import logging
+from typing import NewType
+
+# An integer log level corresponding to a registered log level.
+LevelNo = NewType("LevelNo", int)
 
 
-def to_levelno(level: int | str) -> int:
+def to_levelno(level: int | str) -> LevelNo:
     # Handle `int` level.
     if isinstance(level, int):
         if logging.getLevelName(level).startswith("Level "):
             raise ValueError(f"Unknown level: {level!r}")
-        return level
+        return LevelNo(level)
     # Handle `str` level.
     if isinstance(level, str):
         levelno = logging.getLevelName(level)
         if not isinstance(levelno, int):
             raise ValueError(f"Unknown level: {level!r}")
-        return levelno
+        return LevelNo(levelno)
     # Fail on other types.
     raise TypeError(f"Invalid level: {level!r}")
