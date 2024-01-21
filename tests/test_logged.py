02311705285bbec6be9bebf0f508fd6d1940aebd
Dave Hall <dave@etianen.com>
2024-01-21 21:38:32 +0000

    wip

--- tests/test_logged.py
+++ tests/test_logged.py
@@ -61,7 +61,10 @@ def test_ordered_all_logged_eq_pass() -> None:
 
 
 def test_ordered_all_logged_eq_fail() -> None:
+    # Different orderings are not equal.
     assert (logged.info("foo") >> logged.info("bar")) != (logged.info("bar") >> logged.info("foo"))
+    # Different operators are not equal.
+    assert (logged.info("foo") >> logged.info("bar")) != (logged.info("foo") & logged.info("bar"))
 
 
 def test_ordered_all_logged_repr() -> None:
