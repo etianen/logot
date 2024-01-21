939ea2f3a9d0b761297db3d6f87a711f8392b5bb
Dave Hall <dave@etianen.com>
2024-01-21 19:57:03 +0000

    wip

--- tests/test_logged.py
+++ tests/test_logged.py
@@ -10,3 +10,7 @@ def test_log_record_logged_eq_pass() -> None:
 def test_log_record_logged_eq_fail() -> None:
     assert logged.info("foo bar") != logged.debug("foo bar")
     assert logged.info("foo bar") != logged.info("foo")
+
+
+def test_log_record_logged_repr() -> None:
+    assert repr(logged.info("foo bar")) == "log('INFO', 'foo bar')"
