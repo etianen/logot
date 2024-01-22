057cbf63517a9d5e8689caccdc0d007ba2cfb8ed
Dave Hall <dave@etianen.com>
2024-01-22 21:35:18 +0000

    wip

--- tests/test_logot.py
+++ tests/test_logot.py
@@ -13,6 +13,10 @@ def test_capturing() -> None:
     try:
         with Logot().capturing(level=logging.DEBUG, logger=logger) as logot:
             assert isinstance(logot, Logot)
+            # The logger will have been overridden for the required verbosity.
+            assert logger.level == logging.DEBUG
+        # When the capture ends, the logging verbosity is restored.
+        assert logger.level == logging.WARNING
     finally:
         # Whatever this test does, reset the logger to what it was!
         logger.setLevel(logging.NOTSET)
