50303cc8e9b744090ad48af6302e4b4a51a5b70e
Dave Hall <dave@etianen.com>
2024-01-22 13:11:46 +0000

    wip

--- logot/__init__.py
+++ logot/__init__.py
@@ -6,5 +6,10 @@ import logging
 class Logot:
     __slots__ = ()
 
-    def __init__(self, *, logger: logging.Logger | str | None) -> None:
+    def __init__(
+        self,
+        *,
+        level: int | str = logging.NOTSET,
+        logger: logging.Logger | str | None = None,
+    ) -> None:
         pass
