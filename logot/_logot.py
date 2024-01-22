ef023453ac558a346cd4d3f46dee4c050bec4e9c
Dave Hall <dave@etianen.com>
2024-01-22 21:22:18 +0000

    wip

--- logot/_logot.py
+++ logot/_logot.py
@@ -39,7 +39,7 @@ class Logot:
         level: int | str = DEFAULT_LEVEL,
         logger: logging.Logger | str | None = DEFAULT_LOGGER,
     ) -> AbstractContextManager[Logot]:
-        return _Capturing(self, levelno=to_levelno(level), logger=to_logger(logger))
+        return _Capturing(self, _Handler(self, levelno=to_levelno(level)), logger=to_logger(logger))
 
     def assert_logged(self, log: Logged) -> None:
         reduced_log = self._reduce(log)
@@ -121,20 +121,19 @@ class Logot:
 
 
 class _Capturing:
-    __slots__ = ("_logot", "_levelno", "_logger", "_prev_levelno", "_handler")
+    __slots__ = ("_logot", "_handler", "_logger", "_prev_levelno")
 
-    def __init__(self, logot: Logot, *, levelno: int, logger: logging.Logger) -> None:
+    def __init__(self, logot: Logot, handler: logging.Handler, *, logger: logging.Logger) -> None:
         self._logot = logot
-        self._levelno = levelno
+        self._handler = handler
         self._logger = logger
 
     def __enter__(self) -> Logot:
         # If the logger is less verbose than the handler, force it to the necessary verboseness.
         self._prev_levelno = self._logger.level
-        if self._levelno < self._logger.level:
-            self._logger.setLevel(self._levelno)
+        if self._handler.level < self._logger.level:
+            self._logger.setLevel(self._handler.level)
         # Add the handler.
-        self._handler = _Handler(self._logot, levelno=self._levelno)
         self._logger.addHandler(self._handler)
         return self._logot
 
