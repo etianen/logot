4517927a9de6ebfe293b15aef461cf26eaf053cb
Dave Hall <dave@etianen.com>
2024-01-22 21:52:54 +0000

    wip

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,21 @@
 from __future__ import annotations
 
 import logging
+import threading
+from time import sleep
 
 logger = logging.getLogger("logot")
 
 
 def lines(*lines: str) -> str:
     return "\n".join(lines)
+
+
+def log_soon(level: int, msg: str) -> None:
+    thread = threading.Thread(target=_log_soon, args=(level, msg))
+    thread.start()
+
+
+def _log_soon(level: int, msg: str) -> None:
+    sleep(0.1)
+    logger.log(level, msg)
