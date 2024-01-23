790a966f82f89ee7976439969d7a9569f1ef79a0
Dave Hall <dave@etianen.com>
2024-01-23 01:18:35 +0000

    wip

--- logot/_logot.py
+++ logot/_logot.py
@@ -67,23 +67,35 @@ class Logot:
         return _Capturing(self, _Handler(self, levelno=levelno), logger=logger)
 
     def assert_logged(self, log: Logged) -> None:
+        """
+        Fails *immediately* if the expected ``log`` pattern has not arrived.
+
+        :param log: The expected :doc:`log pattern <logged>`.
+        :raises AssertionError: If the expected ``log`` pattern has not arrived.
+        """
         reduced_log = self._reduce(log)
         if reduced_log is not None:
             raise AssertionError(f"Not logged:\n\n{reduced_log}")
 
     def assert_not_logged(self, log: Logged) -> None:
+        """
+        Fails *immediately* if the expected ``log`` pattern **has** arrived.
+
+        :param log: The expected :doc:`log pattern <logged>`.
+        :raises AssertionError: If the expected ``log`` pattern **has** arrived.
+        """
         reduced_log = self._reduce(log)
         if reduced_log is None:
             raise AssertionError(f"Logged:\n\n{log}")
 
     def wait_for(self, log: Logged, *, timeout: float | None = None) -> None:
         """
-        Waits for the expected ``log`` to arrive or the ``timeout`` to expire.
+        Waits for the expected ``log`` pattern to arrive or the ``timeout`` to expire.
 
         :param log: The expected :doc:`log pattern <logged>`.
         :param timeout: How long to wait (in seconds) before failing the test. Defaults to the ``timeout`` passed to
             :class:`Logot`.
-        :raises AssertionError: If the expected ``log`` does not arrive within ``timeout`` seconds.
+        :raises AssertionError: If the expected ``log`` pattern does not arrive within ``timeout`` seconds.
         """
         waiter = self._open_waiter(log, SyncWaiter, timeout=timeout)
         try:
@@ -93,12 +105,12 @@ class Logot:
 
     async def await_for(self, log: Logged, *, timeout: float | None = None) -> None:
         """
-        Waits *asynchronously* for the expected ``log`` to arrive or the ``timeout`` to expire.
+        Waits *asynchronously* for the expected ``log`` pattern to arrive or the ``timeout`` to expire.
 
         :param log: The expected :doc:`log pattern <logged>`.
         :param timeout: How long to wait (in seconds) before failing the test. Defaults to the ``timeout`` passed to
             :class:`Logot`.
-        :raises AssertionError: If the expected ``log`` does not arrive within ``timeout`` seconds.
+        :raises AssertionError: If the expected ``log`` pattern does not arrive within ``timeout`` seconds.
         """
         waiter = self._open_waiter(log, AsyncWaiter, timeout=timeout)
         try:
