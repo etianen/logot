ddfc7220a663b493e4af5e72c3554c43c6393646
Dave Hall <dave@etianen.com>
2024-01-29 15:58:38 +0000

    wip

--- tests/test_format.py
+++ tests/test_format.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
 
-import logging
-
 from logot._format import format_log
 
 
 def test_format_log() -> None:
-    assert format_log(logging.INFO, "foo bar") == "[INFO] foo bar"
+    assert format_log("INFO", "foo bar") == "[INFO] foo bar"
