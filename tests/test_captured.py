fed761cc76f30e86c0fee13b7f42a8f37eae186b
Dave Hall <dave@etianen.com>
2024-01-29 20:48:02 +0000

    wip

--- tests/test_captured.py
+++ tests/test_captured.py
@@ -6,21 +6,21 @@ from logot import Captured
 
 
 def test_eq_pass() -> None:
-    assert Captured("INFO", "foo bar", levelno=logging.INFO) == Captured("INFO", "foo bar", levelno=logging.INFO)
+    assert Captured("INFO", "foo bar") == Captured("INFO", "foo bar")
 
 
 def test_eq_fail() -> None:
     # Different levelnames are not equal.
-    assert Captured("INFO", "foo bar", levelno=logging.INFO) != Captured("DEBUG", "foo bar", levelno=logging.INFO)
+    assert Captured("INFO", "foo bar") != Captured("DEBUG", "foo bar")
     # Different messages are not equal.
-    assert Captured("INFO", "foo bar", levelno=logging.INFO) != Captured("INFO", "foo", levelno=logging.INFO)
+    assert Captured("INFO", "foo bar") != Captured("INFO", "foo")
     # Different levelnos are not equal.
-    assert Captured("INFO", "foo bar", levelno=logging.INFO) != Captured("INFO", "foo bar", levelno=logging.DEBUG)
+    assert Captured("INFO", "foo bar") != Captured("INFO", "foo bar", levelno=logging.DEBUG)
 
 
 def test_repr() -> None:
-    assert repr(Captured("INFO", "foo bar", levelno=99)) == "Captured('INFO', 'foo bar', levelno=99)"
+    assert repr(Captured("INFO", "foo bar")) == "Captured('INFO', 'foo bar', levelno=None)"
 
 
 def test_str() -> None:
-    assert str(Captured("INFO", "foo bar", levelno=logging.INFO)) == "[INFO] foo bar"
+    assert str(Captured("INFO", "foo bar")) == "[INFO] foo bar"
