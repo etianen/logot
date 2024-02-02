8faa7211fa965fa1f04e708c9e42d8f979fa27f7
Dave Hall <dave@etianen.com>
2024-02-02 20:35:50 +0000

    wip

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -2,14 +2,17 @@ from __future__ import annotations
 
 import pytest
 
+from logot import Logot
+from logot._types import Level, LoggerLike
 
-def test_config_default(pytester: pytest.Pytester) -> None:
-    pytester.makepyfile(
-        """
-        def test_level(logot_level):
-            assert logot_level == "DEBUG"
 
-        def test_logger(logot_logger):
-            assert logot_logger is None
-        """
-    )
+def test_level_default(logot_level: Level) -> None:
+    assert logot_level == Logot.DEFAULT_LEVEL
+
+
+def test_logger_default(logot_logger: LoggerLike) -> None:
+    assert logot_logger == Logot.DEFAULT_LOGGER
+
+
+def test_timeout_default(logot_timeout: float) -> None:
+    assert logot_timeout == Logot.DEFAULT_TIMEOUT
