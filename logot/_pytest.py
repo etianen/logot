1e10ebf8104473b3f9a17e4f17a3305e4a439605
Dave Hall <dave@etianen.com>
2024-02-02 20:04:03 +0000

    wip

--- logot/_pytest.py
+++ logot/_pytest.py
@@ -1,11 +1,15 @@
 from __future__ import annotations
 
 from collections.abc import Generator
-from typing import Any
+from typing import Any, Callable, TypeVar
 
 import pytest
 
 from logot._logot import Logot
+
+T = TypeVar("T")
+
+MISSING: Any = object()
 
 
 def pytest_addoption(parser: pytest.Parser, pluginmanager: pytest.PytestPluginManager) -> None:
@@ -14,21 +18,18 @@ def pytest_addoption(parser: pytest.Parser, pluginmanager: pytest.PytestPluginMa
         parser,
         group,
         name="level",
-        default=Logot.DEFAULT_LEVEL,
         help="Level used for automatic `logot` log capturing",
     )
     _addoption(
         parser,
         group,
         name="logger",
-        default=Logot.DEFAULT_LOGGER,
         help="Logger used for automatic `logot` log capturing",
     )
     _addoption(
         parser,
         group,
         name="timeout",
-        default=Logot.DEFAULT_TIMEOUT,
         help="Default timeout (in seconds) for `logot`",
     )
 
@@ -39,16 +40,33 @@ def logot() -> Generator[Logot, None, None]:
         yield logot
 
 
-def _addoption(parser: pytest.Parser, group: pytest.OptionGroup, *, name: str, default: Any, help: str) -> None:
+@pytest.fixture()
+def logot_level(request: pytest.FixtureRequest) -> str:
+    return _getoption(request.config, name="level", parser=str, default=Logot.DEFAULT_LEVEL)
+
+
+def _addoption(parser: pytest.Parser, group: pytest.OptionGroup, *, name: str, help: str) -> None:
     qualname = f"logot_{name}"
-    parser.addini(
-        qualname,
-        help=help,
-        default=str(default),
-    )
+    parser.addini(qualname, default=MISSING, help=help)
     group.addoption(
         f"--logot-{name.replace('_', '-')}",
         dest=qualname,
         metavar=name.upper(),
         help=help,
     )
+
+
+def _getoption(request: pytest.FixtureRequest, *, name: str, parser: Callable[[str], T], default: T) -> T:
+    qualname = f"logot_{name}"
+    # Try to get the value from the command line, followed by the config file.
+    value: str = request.config.getoption(qualname, default=MISSING)
+    if value is MISSING:
+        value = request.config.getini(qualname)
+        if value is MISSING:
+            # Give up and return the default.
+            return default
+    # Parse and return the value.
+    try:
+        return parser(value)
+    except (ValueError, TypeError) as ex:
+        raise pytest.UsageError(f"Invalid {qualname}: {ex}")
