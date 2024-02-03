80403c81720f795ddd1f242be977efbad1ca34cb
Dave Hall <dave@etianen.com>
2024-02-03 15:33:04 +0000

    wip

--- logot/_typing.py
+++ logot/_typing.py
@@ -1,1 +1,8 @@
 from __future__ import annotations
+
+import sys
+
+if sys.version_info >= (3, 10):
+    from typing import TypeAlias as TypeAlias
+else:
+    from typing_extensions import TypeAlias as TypeAlias
