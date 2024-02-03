858f2db3f45e9f5fe7912e86489048ea5324450d
Dave Hall <dave@etianen.com>
2024-02-03 15:35:00 +0000

    wip

--- logot/_typing.py
+++ logot/_typing.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 
 import sys
+from typing import TypeVar
 
 if sys.version_info >= (3, 10):
+    from typing import ParamSpec as ParamSpec
     from typing import TypeAlias as TypeAlias
 else:
+    from typing_extensions import ParamSpec as ParamSpec
     from typing_extensions import TypeAlias as TypeAlias
+
+T = TypeVar("T")
