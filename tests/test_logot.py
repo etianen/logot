06c06a2838b6984a659f42875342153be58927ec
Dave Hall <dave@etianen.com>
2024-02-04 15:31:44 +0000

    wip

--- tests/test_logot.py
+++ tests/test_logot.py
@@ -5,7 +5,7 @@ import logging
 import pytest
 
 from logot import Captured, Logot, logged
-from tests import capture_soon, lines, logger
+from tests import lines, logger
 
 
 def test_capturing() -> None:
@@ -54,27 +54,6 @@ def test_assert_not_logged_fail(logot: Logot) -> None:
         logot.assert_not_logged(logged.info("foo bar"))
     assert str(ex.value) == lines(
         "Logged:",
-        "",
-        "[INFO] foo bar",
-    )
-
-
-def test_wait_for_pass_immediate(logot: Logot) -> None:
-    logot.capture(Captured("INFO", "foo bar"))
-    logot.wait_for(logged.info("foo bar"))
-
-
-def test_wait_for_pass_soon(logot: Logot) -> None:
-    with capture_soon(logot, Captured("INFO", "foo bar")):
-        logot.wait_for(logged.info("foo bar"))
-
-
-def test_wait_for_fail(logot: Logot) -> None:
-    logot.capture(Captured("INFO", "boom!"))
-    with pytest.raises(AssertionError) as ex:
-        logot.wait_for(logged.info("foo bar"), timeout=0.1)
-    assert str(ex.value) == lines(
-        "Not logged:",
         "",
         "[INFO] foo bar",
     )
