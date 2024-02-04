4f4b18a7a806e7003be9b64c479c9e1cc3c27b0e
Dave Hall <dave@etianen.com>
2024-02-04 15:30:29 +0000

    wip

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,26 +1,14 @@
 from __future__ import annotations
 
-import asyncio
 import logging
 import threading
-from collections.abc import Coroutine, Generator
+from collections.abc import Generator
 from contextlib import contextmanager
-from functools import wraps
 from time import sleep
-from typing import Any, Callable
 
 from logot import Captured, Logot
-from logot._typing import P
 
 logger = logging.getLogger("logot")
-
-
-def asyncio_test(test_fn: Callable[P, Coroutine[Any, Any, None]]) -> Callable[P, None]:
-    @wraps(test_fn)
-    def asyncio_test_wrapper(*args: P.args, **kwargs: P.kwargs) -> None:
-        asyncio.run(test_fn(*args, **kwargs))
-
-    return asyncio_test_wrapper
 
 
 def lines(*lines: str) -> str:
---
+++ tests/test_asyncio.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import asyncio
+from collections.abc import Coroutine
+from functools import wraps
+from typing import Any, Callable
+
+import pytest
+
+from logot import Captured, Logot, logged
+from logot._typing import P
+from tests import capture_soon, lines
+
+
+def asyncio_test(test_fn: Callable[P, Coroutine[Any, Any, None]]) -> Callable[P, None]:
+    @wraps(test_fn)
+    def asyncio_test_wrapper(*args: P.args, **kwargs: P.kwargs) -> None:
+        asyncio.run(test_fn(*args, **kwargs))
+
+    return asyncio_test_wrapper
+
+
+@asyncio_test
+async def test_await_for_pass_immediate(logot: Logot) -> None:
+    logot.capture(Captured("INFO", "foo bar"))
+    await logot.await_for(logged.info("foo bar"))
+
+
+@asyncio_test
+async def test_await_for_pass_soon(logot: Logot) -> None:
+    with capture_soon(logot, Captured("INFO", "foo bar")):
+        await logot.await_for(logged.info("foo bar"))
+
+
+@asyncio_test
+async def test_await_for_fail(logot: Logot) -> None:
+    logot.capture(Captured("INFO", "boom!"))
+    with pytest.raises(AssertionError) as ex:
+        await logot.await_for(logged.info("foo bar"), timeout=0.1)
+    assert str(ex.value) == lines(
+        "Not logged:",
+        "",
+        "[INFO] foo bar",
+    )
