e61ffdf751b99d0e9b0aa954a37d0b382d6682a7
Dave Hall <dave@etianen.com>
2024-02-06 15:20:19 +0000

    wip

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from contextvars import ContextVar
 from shlex import quote
 from typing import Any, Callable
 
@@ -7,81 +8,76 @@ import pytest
 
 from logot import Logot
 from logot._pytest import get_optname, get_qualname
+from logot._typing import MISSING
 from logot._wait import AsyncWaiter
 from logot.asyncio import AsyncioWaiter
 
+EXPECTED_VAR: ContextVar[Any] = ContextVar(f"{__name__}.EXPECTED_VAR")
 
-def assert_fixture_ini(pytester: pytest.Pytester, name: str, value: Any, *, passed: bool = True) -> None:
+
+def assert_fixture_config(
+    pytester: pytest.Pytester,
+    name: str,
+    value: Any,
+    *,
+    expected: Any = MISSING,
+    passed: bool = True,
+) -> None:
     qualname = get_qualname(name)
     pytester.makepyfile(
         f"""
-        def test_{name}({qualname}):
-            assert {qualname} == {value!r}
-        """
-    )
-    pytester.makeini(
-        f"""
-        [pytest]
-        {qualname} = {value}
-        """
-    )
-    # Run the pytest.
-    result = pytester.runpytest()
-    result.assert_outcomes(passed=int(passed), errors=int(not passed))
+        from tests.test_pytest import EXPECTED_VAR
 
-
-def assert_fixture_cli(pytester: pytest.Pytester, name: str, value: Any, *, passed: bool = True) -> None:
-    qualname = get_qualname(name)
-    pytester.makepyfile(
-        f"""
         def test_{name}({qualname}):
-            assert {qualname} == {value!r}
+            assert {qualname} == EXPECTED_VAR.get()
         """
     )
-    # Run the pytest.
-    result = pytester.runpytest(f"{get_optname(name)}={quote(str(value))}")
-    result.assert_outcomes(
-        passed=int(passed),
-        errors=int(not passed),
-    )
+    # Set the expected `ContextVar`.
+    if expected is MISSING:
+        expected = value
+    expected_token = EXPECTED_VAR.set(expected)
+    try:
+        # Run the pytest with a CLI option.
+        result = pytester.runpytest(f"{get_optname(name)}={quote(str(value))}")
+        result.assert_outcomes(passed=int(passed), errors=int(not passed))
+        # Run the pytest with an INI option.
+        pytester.makeini(
+            f"""
+            [pytest]
+            {qualname} = {value}
+            """
+        )
+        # Run the pytest.
+        result = pytester.runpytest()
+        result.assert_outcomes(passed=int(passed), errors=int(not passed))
+    finally:
+        # Reset the expected `ContextVar`.
+        EXPECTED_VAR.reset(expected_token)
 
 
 def test_level_default(logot_level: str | int) -> None:
     assert logot_level == Logot.DEFAULT_LEVEL
 
 
-def test_level_ini(pytester: pytest.Pytester) -> None:
-    assert_fixture_ini(pytester, "level", "INFO")
-
-
-def test_level_cli(pytester: pytest.Pytester) -> None:
-    assert_fixture_cli(pytester, "level", "INFO")
+def test_level_config(pytester: pytest.Pytester) -> None:
+    assert_fixture_config(pytester, "level", "INFO")
 
 
 def test_logger_default(logot_logger: str | int) -> None:
     assert logot_logger == Logot.DEFAULT_LOGGER
 
 
-def test_logger_ini(pytester: pytest.Pytester) -> None:
-    assert_fixture_ini(pytester, "logger", "logot")
-
-
-def test_logger_cli(pytester: pytest.Pytester) -> None:
-    assert_fixture_cli(pytester, "logger", "logot")
+def test_logger_config(pytester: pytest.Pytester) -> None:
+    assert_fixture_config(pytester, "logger", "logot")
 
 
 def test_timeout_default(logot_timeout: float) -> None:
     assert logot_timeout == Logot.DEFAULT_TIMEOUT
 
 
-def test_timeout_ini(pytester: pytest.Pytester) -> None:
-    assert_fixture_ini(pytester, "timeout", 9999.0)
-    assert_fixture_ini(pytester, "timeout", "boom!", passed=False)
-
-
-def test_timeout_cli(pytester: pytest.Pytester) -> None:
-    assert_fixture_cli(pytester, "timeout", 9999.0)
-    assert_fixture_cli(pytester, "timeout", "boom!", passed=False)
+def test_timeout_config(pytester: pytest.Pytester) -> None:
+    assert_fixture_config(pytester, "timeout", 9999.0)
+    assert_fixture_config(pytester, "timeout", "boom!", passed=False)
 
 
 def test_async_waiter_default(logot_async_waiter: Callable[[], AsyncWaiter]) -> None:
