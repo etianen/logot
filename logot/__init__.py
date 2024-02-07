4ace8296e3f4397fde8ef3ccc56b283c12a6cbb3
Dave Hall <dave@etianen.com>
2024-02-07 20:50:52 +0000

    wip

--- logot/__init__.py
+++ logot/__init__.py
@@ -9,6 +9,5 @@ from __future__ import annotations
 
 from logot._capture import Captured as Captured
 from logot._logged import Logged as Logged
-from logot._logot import Capturer as Capturer
 from logot._logot import Logot as Logot
 from logot._wait import AsyncWaiter as AsyncWaiter
