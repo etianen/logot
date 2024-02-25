fd410eb80649d410286b6a00037a726b307be8d9
Dave Hall <dave@etianen.com>
2024-02-25 13:22:29 +0000

    wip

--- tests/test_level.py
+++ tests/test_level.py
@@ -1,6 +1,11 @@
 from __future__ import annotations
 
+from typing import cast
+
+import pytest
+
 from logot._level import level_matcher
+from logot._typing import Level
 
 
 def test_eq_pass() -> None:
@@ -22,3 +27,9 @@ def test_repr() -> None:
 def test_str() -> None:
     assert str(level_matcher("INFO")) == "[INFO]"
     assert str(level_matcher(20)) == "[Level 20]"
+
+
+def test_invalid() -> None:
+    with pytest.raises(TypeError) as ex:
+        level_matcher(cast(Level, 1.5))
+    assert str(ex.value) == "Invalid level: 1.5"
