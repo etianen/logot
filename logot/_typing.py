e55c1cc0767ec2c2f8e96fb787f842cac47f3121
Dave Hall <dave@etianen.com>
2025-07-28 12:05:21 +0100

    wip

--- logot/_typing.py
+++ logot/_typing.py
@@ -1,14 +1,17 @@
 from __future__ import annotations
 
 import sys
-from typing import TYPE_CHECKING, TypeVar, Union
+from typing import TYPE_CHECKING, Any, TypeVar, Union
 
 if sys.version_info >= (3, 10):
+    from types import EllipsisType
     from typing import ParamSpec as ParamSpec
     from typing import TypeAlias as TypeAlias
 else:
     from typing_extensions import ParamSpec as ParamSpec
     from typing_extensions import TypeAlias as TypeAlias
+
+    EllipsisType: TypeAlias = Any
 
 P = ParamSpec("P")
 T = TypeVar("T")
@@ -18,9 +21,8 @@ Level: TypeAlias = Union[str, int]
 Name: TypeAlias = Union[str, None]
 
 if TYPE_CHECKING:  # pragma: no cover
-    from types import EllipsisType
-
-    Wildcard: TypeAlias = T | EllipsisType
+    # TODO: Use `UnionType` when we only need to support Python 3.10+.
+    Wildcard: TypeAlias = Union[T, Any]
 else:  # pragma: no cover
     # Hide `| EllipsisType` from the docs.
     class Wildcard:
