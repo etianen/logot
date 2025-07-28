9531619af22d52946556fb403368ebccc9a00d0c
Dave Hall <dave@etianen.com>
2025-07-28 11:58:27 +0100

    wip

--- logot/_typing.py
+++ logot/_typing.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import sys
-from typing import TYPE_CHECKING, TypeAlias, TypeVar, Union
+from typing import TYPE_CHECKING, TypeVar, Union
 
 if sys.version_info >= (3, 10):
     from typing import ParamSpec as ParamSpec
