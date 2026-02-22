798b8c7aab5b97ff935b361f4b776bd10a4b0d4f
Dave Hall <dave@etianen.com>
2026-02-22 20:27:43 +0000

    Added support for `pytest` v9, dropped support for Python 3.9 (#325)

--- logot/_typing.py
+++ logot/_typing.py
@@ -1,28 +1,18 @@
 from __future__ import annotations
 
-import sys
-from typing import TYPE_CHECKING, Any, TypeVar, Union
-
-if sys.version_info >= (3, 10):
-    from types import EllipsisType
-    from typing import ParamSpec as ParamSpec
-    from typing import TypeAlias as TypeAlias
-else:
-    from typing_extensions import ParamSpec as ParamSpec
-    from typing_extensions import TypeAlias as TypeAlias
-
-    EllipsisType: TypeAlias = Any
+from types import EllipsisType
+from typing import TYPE_CHECKING, TypeVar
+from typing import ParamSpec as ParamSpec
+from typing import TypeAlias as TypeAlias
 
 P = ParamSpec("P")
 T = TypeVar("T")
 
-# TODO: Use `UnionType` when we only need to support Python 3.10+.
-Level: TypeAlias = Union[str, int]
-Name: TypeAlias = Union[str, None]
+Level: TypeAlias = str | int
+Name: TypeAlias = str | None
 
 if TYPE_CHECKING:  # pragma: no cover
-    # TODO: Use `UnionType` when we only need to support Python 3.10+.
-    Wildcard: TypeAlias = Union[T, EllipsisType]
+    Wildcard: TypeAlias = T | EllipsisType
 else:  # pragma: no cover
     # Hide `| EllipsisType` from the docs.
     class Wildcard:
